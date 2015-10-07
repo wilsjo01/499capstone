9887610464fed3d414f885bcc4a5b0fcb569af31
madeline.marx <madeline.marx@bluestembrands.com>
2015-10-06 20:14:45 -0500

    fixes

--- Game.php
+++ Game.php
@@ -7,29 +7,7 @@
 	* Based on user input the data will be Appended to the end of the file.
 	*****************************************/
 	session_start();	
-	require ("template_Top.php");
-?>
 	
-	<p id='story'> 
-		Metro State Story
-	</p> 
-	<form name="start" method="post" title="Choose Character" action='Functions.php'> 
-		<table> 
-			<tr> <b> Select Story Option </b>
-				<td> Choice One
-				<input type='radio' name="choice" value="McNally">
-				</td>
-				<td> Choice Two
-				<input type='radio' name="choice" value="Capitol">
-				</td>
-				<td> 
-				<input type='submit' name="submit">
-				</td>				
-			</tr>
-		</table>
-	</form>
-<?php 
-	require ("template_Bottom.php"); 
 		//############
 	//# FUNCTIONS
 	//############
@@ -140,4 +118,3 @@
 				</td>
 			</tr>
 		</table>	
-<?php require ("template_Bottom.php"); ?>
