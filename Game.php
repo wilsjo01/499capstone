2320da52e77f20ecffc8186a6b918a05ac919f83
Brandon Markgraf <brandon.j.markgraf@gmail.com>
2015-10-11 13:10:13 -0500

    Start Game/Game Updates
    These updates include playing through Game One statically and also take
    variables from Start_Game.php and pass them in Game.php to be used to
    display game story and character name that's selected.

--- Game.php
+++ Game.php
@@ -1,16 +1,15 @@
+<!-- ================================================================================================================== -->
 <?php 
-	//Code to Validate the Registration form submission - START
 	/*****************************************
-	* Author:  	Brandon Markgraf
-	* Date: 	2/17/14
-	* Description: This file updates a flat text file with customer Information.  
-	* Based on user input the data will be Appended to the end of the file.
+	* Author:  	
+	* Date: 	
+	* Description: 
 	*****************************************/
 	session_start();	
 	
-		//############
-	//# FUNCTIONS
-	//############
+	#############
+	# FUNCTIONS #
+	#############
 	Function test_input ($data) {
 		$data = trim($data);			//Strip unnecessary characters (extra space, tab, newline)
 		$data = stripslashes($data);	//remove backslashes (\)
@@ -20,77 +19,142 @@
 		Return $data;
 	}
 	
-	Function Print_Game_Choices ($page) {
-		global $OptOneStories, $OptTwoStories;
-
+	Function Print_Game_Choices () {
 		echo "<form name=\"Adventure Choice\" method=\"post\" action=\"".$_SERVER['PHP_SELF']."\">";
-			echo "<h3> Please Select Your Next Path:<br /></h3>";
-			echo "<input type=\"radio\" name=\"choice\" value=\"Option1\">".$OptOneStories[$page][1]."<br />";
-			echo "<input type=\"radio\" name=\"choice\" value=\"Option2\">".$OptTwoStories[$page][1]."<br />";
-			echo "<input type=\"hidden\" name=\"page\" value=\"".++$page."\">";
-			echo "<input type=\"submit\" value=\"Submit\" name=\"Submit\" style=\"width:100px\">";
+			echo "<h3>Which Path Do You Choose:<br /></h3>";
+			echo "<input type=\"radio\" name=\"choice\" value=\"Option1\" checked>".$_SESSION['OptOneStories'][$_SESSION['page']][1]."<br />";
+			echo "<input type=\"radio\" name=\"choice\" value=\"Option2\">".$_SESSION['OptTwoStories'][$_SESSION['page']][1]."<br />";
+			echo "<br />";
+			echo "<input type=\"submit\" value=\"Submit Choice\" name=\"internalSubmit\" style=\"width:100px\">";			
+			echo "<input type=\"submit\" value=\"RESET Game\" name=\"ResetGame\" style=\"width:100px\">";
 		echo "</form>";
 	}
 	
-	//########
-	//# MAIN
-	//########
-
-	$OptOneStories= array (
-		array(1,"McNally","McNally.txt"),
-		array(2,"Concordia","Concordia.txt"),
-		array(3,"Augsburg","Augsburg.txt"),
-		array(4,"Foshay","Foshay.txt"),
-		array(5,"First Ave","FirstAve.txt")
-	);
+	#How the story array's are setup.
+	#array(x,[Location],[Story File],[Story Continue; 0=Yes/1=No]);
 	
-	$OptTwoStories = array (
-		array(1,"Capitol","Capitol.txt"),
-		array(2,"Midway","Midway.txt"),
-		array(3,"UofM","UofM.txt"),
-		array(4,"Hennepin County","Hennepin_County.txt"),
-		array(5,"Target Center","Target_Center.txt")
-	);
+	Function Game1 () {
+		#Option One Stories
+		$_SESSION['OptOneStories'] = array (
+			array(1,"McNally","McNally.txt",0),
+			array(2,"Concordia","Concordia.txt",0),
+			array(3,"Augsburg","Augsburg.txt",0),
+			array(4,"Foshay","Foshay.txt",1),
+			array(5,"First Ave","FirstAve.txt",0)
+		);
+		#Option Two Stories
+		$_SESSION['OptTwoStories'] = array (
+			array(1,"Capitol","Capitol.txt",0),
+			array(2,"Midway","Midway.txt",1),
+			array(3,"UofM","UofM.txt",0),
+			array(4,"Hennepin County","Hennepin_County.txt",0),
+			array(5,"Target Center","Target_Center.txt",1)
+		);
+	}
 	
-	if(isset($_POST['Submit'])) {		
-		$choice = test_input($_POST["choice"]);
-		$page = ($_POST["page"]);
-		$i = $page - 1;
+	Function Game2 () {
 		
-		if ($page <= 4) {
+	}
+	
+	Function Game3 () {
+		
+	}
+	
+	Function Game4 () {
+		
+	}
+	
+	Function Game5 () {
+		
+	}
+	
+	Function Game6 () {
+		
+	}
+	
+	#########
+	# MAIN  #
+	#########	
+	
+	#Action(s) for Initial Start Game Button from Start_Game.php
+	if(isset($_POST['StartGameSubmit'])) {
+		$_SESSION['Game'] = test_input($_POST["GameSelection"]);
+		$_SESSION['character'] = test_input($_POST["CharSelection"]);
+		$_SESSION['story_base'] = file_get_contents('./Game_Stories/Game1/Metro_State.txt', true);
+		$_SESSION['page'] = 0;
+		
+		switch ($_SESSION['Game']) {
+			case "Game1":
+				Game1 ();
+				break;
+			case "Game1":
+				Game2 ();
+				break;
+			case "Game1":
+				Game3 ();
+				break;
+			case "Game1":
+				Game4 ();
+				break;
+			case "Game1":
+				Game5 ();
+				break;
+			case "Game1":
+				Game6 ();
+				break;
+		}
+	}
+	
+	#Action(s) for Reset Game Button
+	if (isset($_POST['ResetGame'])) {
+		$_SESSION['page'] = 0;
+	}
+	
+	#Action(s) for Internal Submit Button for Story Selection
+	if(isset($_POST['internalSubmit'])) {
+		$choice = test_input($_POST["choice"]);
+		
+		++$_SESSION['page'];
+		$i = $_SESSION['page'] - 1;		
+		
+		if ($_SESSION['page'] <= 4) {
 			#Get the Story the User Selected
 			if ($choice == "Option1") {
-				$story = file_get_contents('./Game_Stories/Game1/'.$OptOneStories[$i][2], true);
+				$story = file_get_contents('./Game_Stories/Game1/'.$_SESSION['OptOneStories'][$i][2], true);
+				$story_over = $_SESSION['OptOneStories'][$i][3];
 			} else if ($choice == "Option2") {
-				$story = file_get_contents('./Game_Stories/Game1/'.$OptTwoStories[$i][2], true);
+				$story = file_get_contents('./Game_Stories/Game1/'.$_SESSION['OptTwoStories'][$i][2], true);
+				$story_over = $_SESSION['OptTwoStories'][$i][3];
 			}
 		} else {
 			$post_story = file_get_contents('./Game_Stories/Game1/Train_Station.txt', true);
 		}
-	} else {
-		$story_base = file_get_contents('./Game_Stories/Game1/Metro_State.txt', true);
-		$page = 0;
 	}
 ?>
-	
 <!-- ================================================================================================================== -->
 <!-- MAIN HTML Page - START -->
 <?php
 	require ("template_Top.php");
+	
+	echo "<h3>Hello ".$_SESSION['character']."!!! Welcome to the ".$_SESSION['Game']." adventure.</h3>";
 	
 	echo "<p>";
 		if (isset($post_story)) {
 			print $post_story;
 		} else if (isset($story)){
 			print $story;
-			Print_Game_Choices($page);
+			if ($story_over === 0) {
+				Print_Game_Choices();
+			} else {
+				echo "Story over, SORRY!!!!!";
+			}
 		} else {
-			print $story_base;
-			Print_Game_Choices($page);
+			print $_SESSION['story_base'];
+			Print_Game_Choices();
 		}
 	echo "</p>";
-	
-	echo "<a href=\"game.php\">Reset Game</a>";
-	
+
 	require ("template_Bottom.php");
 ?>
+<!-- MAIN HTML Page - END  -->
+<!-- ================================================================================================================== -->
--- Start_Game.php
+++ Start_Game.php
@@ -3,53 +3,42 @@
 	require ("template_Top.php");	
 ?>
 
+<br />
+
+<form name="Start Game" method="post" title="Character Game Selection" action='Game.php'> 
+	<table> 
+		<tr><b>Select a Character</b></tr>
+		<tr> 
+			<td><img src="./images/girl.png" height = 75 width = 45></td>
+			<!-- 
+			<td><img src="./images/girl.png" height = 300 width = 180></td>
+			<td><img src="./images/girl.png" height = 300 width = 180></td>
+			-->
+		</tr>
+		<tr>
+			<td>Sally <input type="radio" name="CharSelection" value="Sally" checked></td>
+			<!-- 
+			<td>Character Two <input type='radio' name="char" value="girl.png"></td>
+			<td>Character Three<input type='radio' name="char" value="girl.png"></td>
+			-->
+		</tr>
+	</table>
 	<br />
-	<p>This is where you'd select the adventure you'd like to play through and your character.</p>
-	<br />
-	<h3> Make Choice </h3> 
-	<form name="start" method="get" title="Choose Character" action='Game.php'> 
-		<table> 
-			<tr> <b> Select Character </b>
-			</tr>
-			<tr> 
-				<td><img src="./images/girl.png" height = 300>
-				</td>
-				<td><img src="./images/girl.png" height = 300>
-				</td>
-				<td><img src="./images/girl.png" height = 300>
-				</td>
-			</tr>
-			<tr>
-				<td> Sally 
-				<input type='radio' name="char" value="sally" checked>
-				</td>
-				<td> Character Two
-				<input type='radio' name="char" value="girl.png">
-				</td>
-				<td> Character Three
-				<input type='radio' name="char" value="girl.png"> 
-				</td>
-			</tr>
-		</table>
-		<table >
-			<tr> <b> Select Game </b>
-			</tr>
-			<tr>
-				<td><img src="./images/purple_book.png" height=200 width = 150>
-				</td>
-				<td><img src="./images/green-book-md.png" height=200 width=150 >
-				</td>
-			</tr>
-			<tr>
-				<td> Game One
-				<input type='radio' name="game" value="game1" checked>
-				</td>
-				<td> Game Two
-				<input type='radio' name="game" value="game2">
-				</td>
-			</tr>
-		</table>
-		<input type="Submit" value="Start Game"> 
-	</form>
+	<table>
+		<tr><b> Select Game </b></tr>
+		<tr>
+			<td><img src="./images/purple_book.png" height=50 width = 37></td>
+			<!-- 
+			<td><img src="./images/green-book-md.png" height=200 width=150></td> 
+			-->
+		</tr>
+		<tr>
+			<td>Game One<input type="radio" name="GameSelection" value="Game1" checked></td>
+		</tr>
+	</table>
+	<input type="Submit" value="Start Game" name="StartGameSubmit" style="width:100px">
+</form>
+
+<br />
 
 <?php require ("template_Bottom.php"); ?>
