5b84468043ae42d4d62ee74acc55846cc68fa4f3
madeline.marx <madeline.marx@bluestembrands.com>
2015-11-08 22:58:06 -0600

    Characters Added

--- Start_Game.php
+++ Start_Game.php
@@ -10,6 +10,8 @@
 		<tr><h2>Select a Character</h2></tr>
 		<tr>
 			<div class="cc-selector">
+			<input type="radio" name="CharSelection" value="Tom" id="Tom" checked/>
+        	<label class="drinkcard-cc Tom" for="Tom"></label>
         	<input type="radio" name="CharSelection" value="Sally" id="Sally" checked/>
         	<label class="drinkcard-cc Sally" for="Sally"></label>
         	<input type="radio" name="CharSelection" value="Joe" id="Joe" />
